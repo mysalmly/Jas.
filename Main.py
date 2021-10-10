8ab18d2f530c10f6a7ce889c2935aa85b0a3d1f4
SajHere <87875257+SajHere@users.noreply.github.com>
2021-10-10 18:44:31 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -20,9 +20,9 @@ bot.remove_command("help")
 
 @bot.command()
 async def help(ctx):
-    em = discord.Embed(Title = "Help", Description = "Use the actual goddamned command for more info")
+    em = discord.Embed(Title = "Help", Description = "Use the command for more info")
 
-    em.add_field("Fun", value = "`bored`, `ping`, `kill`, `Saj`, `Soj`, `Jos`, `Kai`, `Forgor`, `nicokawai` ")
+    em.add_field("Other", value = "`ping`, `bored`, the rest are a SECRET")
     em.add_field("Moderation", value = "`DOESNT EXIST yet..")
 
     await ctx.send(embed = em)
@@ -35,47 +35,13 @@ async def ping(ctx):
 
 @bot.command()
 async def bored(ctx):
-    await ctx.send('You are bored? ah well sucky life too bad for you ig saj was bored while writing this too lol i think ill regret putting this here too bad x2 ok wow im so bored i dont think this cmd will even work OR will the bot even get added to IHM like fr WHY am i doing this i also have homework to do right now a sci worksheet ,math and a social project i still need ideas for it too f')
+    await ctx.send("go study kiddo")
 
 
 @bot.command()
-async def everyone(ctx):
-    await ctx.send("@everyone 😳 ")
+async def SuperSecretAdminCMD(ctx):
+    await ctx.send("@everyone")
 
-
-@bot.command()
-async def kill(ctx):
-    await ctx.send("R.I.P janazah time bois n gurls")
-
-
-@bot.command()
-async def soj(ctx):
-    await ctx.send("**Where is Saj?**  Saj is busy :( i miss my master")
-
-
-@bot.command()
-async def kai(ctx):
-    await ctx.send("**Who is kai?**  *mental health.exe has stopped working*")
-
-
-@bot.command()
-async def saj(ctx):
-    await ctx.send("**Who is Saj?**  My master ofc! 😄")
-
-
-@bot.command()
-async def jos(ctx):
-    await ctx.send("**Who is Jos?**  ALL HAIL GRANDMASTER")
-
-
-@bot.command()
-async def forgor(ctx):
-    await ctx.send("**Who is literally everyone else?**  Ask **Them** lols")
-
-
-@bot.command()
-async def nicokawai(ctx):
-    await ctx.send(' ima break your nico nico kneecaps stay halal bois ')
 
 
 @bot.event
